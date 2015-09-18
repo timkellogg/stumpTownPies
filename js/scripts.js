2f0c9b21d6eb0b22eafaa355f1506f1b01baee7d
timkellogg <tim.kellogg@gmail.com>
2015-09-18 09:42:19 -0700

    add dynamic rendering of pizza options

--- js/scripts.js
+++ js/scripts.js
@@ -67,13 +67,51 @@ $(document).ready(function() {
   }
 
   function renderNewPizzaOption () {
-    $("<div class='row'>Pizza</div>").insertBefore('#add-pizza');
+    $("<div class='form-inline' role='form'>" +
+      "<div class='form-group'>" +
+        "<label for='pizza-type'>Type</label>" +
+        "<select class='form-control' id='pizza-type'>" +
+          "<option>Cheese</option>" +
+          "<option>Veggie</option>" +
+          "<option>Everything</option>" +
+          "<option>Stumptown Surpise</option>" +
+        "</select>" +
+      "</div>" +
+      "<div class='form-group'>" +
+        "<label for='pizza-size'>Size</label>" +
+        "<select class='form-control' id='pizza-size'>" +
+          "<option>Personal</option>" +
+          "<option>Small</option>" +
+          "<option>Medium</option>" +
+          "<option>Large</option>" +
+          "<option>Huge</option>" +
+        "</select>" +
+      "</div>" +
+      "<div class='form-group'>" +        
+       "<label class='checkbox-inline'><input type='checkbox' id='extra-cheese' value='extra-cheese'> Extra Cheese</label>" +
+       "<label class='checkbox-inline'><input type='checkbox' id='pepperoni' value='pepperoni'> Pepperoni</label>" +
+       "<label class='checkbox-inline'><input type='checkbox' id='bacon' value='bacon'> Bacon</label>" +
+       "<label class='checkbox-inline'><input type='checkbox' id='pinapple' value='pinapple'> Pinapple</label>" +
+       "<label class='checkbox-inline'><input type='checkbox' id='stuffed-crust' value='stuffed-crust'> Stuffed Crust</label>" +
+      "</div>" +
+      "<button id='add-order' class='btn btn-success'>Add</button>" +
+    "</div>"
+    ).insertBefore('#add-pizza');
+  }
+
+  function renderClearOrderBtn () {
+    $("#user-interface").append("<button id='clear-order' class='btn btn-danger'>Junk Order</button>");
+    $("#clear-order").on("click", function() {
+      emptyUserInterface();
+      renderOrderForm();
+    });
   }
 
   function renderOrderForm () {
     emptyUserInterface();
     renderSubTitle("Select Pizza Options:");
     renderNewPizzaBtn();
+    renderClearOrderBtn();
   }
 
   $("#start-order").on("click", function() {
