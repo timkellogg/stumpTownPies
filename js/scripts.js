9b3a5fbf69fd0c1e3a8b24c9d1b2076e493c266c
timkellogg <tim.kellogg@gmail.com>
2015-09-18 08:34:21 -0700

    add ability to calculate pizza prices

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,30 @@ function Order () {
 
 Order.prototype.addPizza = function (pizza) {
   this.pizzas.push(pizza);
+};
+
+function Pizza (type, size, toppings) {
+  this.size = size, 
+  this.type = type,
+  this.toppings = [toppings];
 }
+
+Pizza.prototype.calculatePrice = function () {
+  var basePrice,
+      toppingsPrice,
+      totalPrice;
+  if (this.size === "personal") {
+    basePrice = 3.00;
+  } else if (this.size === "small") {
+    basePrice = 4.00;
+  } else if (this.size === "medium") {
+    basePrice = 5.00;
+  } else if (this.size === "large") {
+    basePrice = 6.00;
+  } else if (this.size === "huge") {
+    basePrice = 8.00;
+  }
+  toppingsPrice = this.toppings.length * 0.50;
+  totalPrice = toppingsPrice + basePrice;
+  return totalPrice;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -11,3 +11,17 @@ describe("Order", function() {
     expect(order.pizzas).to.eql([pizza]);
   });
 });
+
+describe("Pizza", function() {
+  it("should create a Pizza object with attributes of type and toppings", function() {
+    var pizza = new Pizza("mushroom", "large", "extraCheese");
+    expect(pizza.type).to.equal("mushroom");
+    expect(pizza.size).to.equal("large")
+    expect(pizza.toppings).to.eql(["extraCheese"]);
+  });
+
+  it("should create create a method to calculatePrice", function() {
+    var pizza = new Pizza("mushroom", "large", "extraCheese");
+    expect(pizza.calculatePrice()).to.equal(6.50);
+  });
+});
